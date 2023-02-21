d98826c51b84112df57ac8479a93359da8857ed8
Alejandro Arends <alejandroarends77@gmail.com>
2023-02-21 23:27:05 +0100

    mejorando home

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="flex w-full overflow-hidden relative">
+        <div class="relative w-full flex justify-center items-center">
+            <img src="../assets/BackGround-Trale-header2.png" class="block w-full bg-cover" alt="..." />
+        </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,67 +1,29 @@
 <template>
-    <div class="flex w-full overflow-hidden relative">
-        <div class="relative w-full flex justify-center items-center">
-            <img src="../assets/BackGround-Trale-smaller.jpg" class="block w-full bg-cover" alt="..." />
-            <div class="flex flex-col absolute z-30">
-                <div class="flex flex-col items-center w-96 gap-5 sm:w-full md:w-1/2">
-                    <h5 class="font-[Nunito] italic text-9xl wordmark-color font-bold z-50">Trale</h5>
-                    <p class="font-[Nunito] italic text-4xl text-center text-trale font-bold white-space-nowrap">
-                        Prioritized Task List for Agile Individuals</p>
-                    <div class="flex flex-row items-center z-50 w-full justify-around">
-                        <router-link :to="{ name: 'Register' }" custom v-slot="{ navigate }">
-                            <button
-                                class="text-white text-lg bg-wordmark font-bold px-4 py-2 rounded focus:outline-none focus:shadow-outline hover:bg-trale hover:border-gray-700 hover:text-white duration-200 border-solid border-2 border-white font-[Nunito]"
-                                @click="navigate" role="link" type="submit" title="Registrarse en Trale">
-                                Register New User
-                            </button>
-                        </router-link>
-                        <router-link :to="{ name: 'Login' }" custom v-slot="{ navigate }">
-                            <button
-                                class="text-white text-lg bg-wordmark font-bold px-4 py-2 rounded focus:outline-none focus:shadow-outline  hover:border-gray-700 hover:text-white duration-200 border-solid border-2 border-white font-[Nunito] hover:bg-trale"
-                                @click="navigate" role="link" type="submit" title="Usar Trale">
-                                Start to Use
-                            </button>
-                        </router-link>
-                    </div>
-                </div>
+    <HomeHeader />
+    <div class="flex flex-col justify-center items-center w-full overflow-hidden bg-nav pb-24">
+        <div class="flex flex-col items-center w-96 gap-5 sm:w-full md:w-1/2">
+            <h5 class="font-[Nunito] italic text-9xl wordmark-color font-bold">Trale</h5>
+            <p class="font-[Nunito] italic text-4xl text-center text-trale font-bold white-space-nowrap">
+                Prioritized Task List for Agile Individuals</p>
+            <div class="flex flex-row items-center justify-around gap-5">
+                <router-link :to="{ name: 'Register' }" custom v-slot="{ navigate }">
+                    <button
+                        class="text-white text-lg bg-wordmark font-bold px-3 py-2 rounded focus:outline-none focus:shadow-outline hover:bg-trale hover:border-gray-700 hover:text-white duration-200 border-solid border-2 border-white font-[Nunito]"
+                        @click="navigate" role="link" type="submit" title="Registrarse en Trale">
+                        Register New User
+                    </button>
+                </router-link>
+                <router-link :to="{ name: 'Login' }" custom v-slot="{ navigate }">
+                    <button
+                        class="text-white text-lg bg-wordmark font-bold px-3 py-2 rounded focus:outline-none focus:shadow-outline  hover:border-gray-700 hover:text-white duration-200 border-solid border-2 border-white font-[Nunito] hover:bg-trale"
+                        @click="navigate" role="link" type="submit" title="Usar Trale">
+                        Start to Use
+                    </button>
+                </router-link>
             </div>
         </div>
     </div>
-
-
-    <!-- <div class="relative position-relative w-full h-screen overflow-hidden">
-        <img src="../assets/geopliegue.jpg" class="block w-full h-full object-cover" alt="..." />
-        <div class="relative position-absolute flex flex-col items-center justify-center h-full">
-            <div class="relative position-absolute flex flex-row justify-between h-1/2">
-                <div class="relative position-absolute flex flex-col items-center justify-center">
-                    <h5 class="font-[Nunito] italic text-8xl text-gray-900 font-bold">Trale</h5>
-                    <p class="font-[Nunito] text-2xl text-yellow-500 font-bold white-space-nowrap">
-                        Prioritized Task List for Agile Individuals</p>
-                    <div class="relative position-absolute flex flex-row justify-between">
-                    <router-link :to="{ name: 'Register' }" custom v-slot="{ navigate }">
-                            <button
-                                class="text-white font-bold px-4 mr-5 rounded focus:outline-none focus:shadow-outline hover:bg-gif hover:border-gray-700 hover:text-white duration-200 border-solid border-2 border-white font-[Nunito]"
-                                @click="navigate" role="link" type="submit" title="Registrarse en Trale">
-                                Register New User
-                            </button>
-                        </router-link>
-                        <router-link :to="{ name: 'Login' }" custom v-slot="{ navigate }">
-                            <button
-                                class="text-white font-bold px-4 rounded focus:outline-none focus:shadow-outline hover:bg-gif hover:border-gray-700 hover:text-white duration-200 border-solid border-2 border-white font-[Nunito]"
-                                @click="navigate" role="link" type="submit" title="Usar Trale">
-                                Start to Use
-                            </button>
-                        </router-link>
-                    </div>
-                </div>
-                <div class="relative position-absolute">
-                    <img src="../assets/anima-trale-v2.gif" class="block w-1/2 object-cover rounded" alt="..." />
-                </div>
-            </div>
-        </div>
-    </div> -->
-
-
+    <HomeFooter />
 </template>
 
 
@@ -69,8 +31,11 @@
 <script setup>
 //vue
 
+
 //components
 import HomeNavBar from "../components/HomeNavBar.vue";
+import HomeFooter from "../components/HomeFooter.vue";
+import HomeHeader from "../components/HomeHeader.vue";
 
 //supabase
 
@@ -90,4 +55,9 @@ import HomeNavBar from "../components/HomeNavBar.vue";
 .bg-gif {
     background-color: #445D73;
 }
+
+.bg-nav{
+    background-color:#F3F3F3;
+}
 </style>
+
